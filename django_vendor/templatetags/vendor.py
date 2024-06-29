ac7787e309424c2c392c8117dac9fc1cd750950d
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-06-28 22:22:58 -0300

    test: write tests for the vendor tag

--- django_vendor/templatetags/vendor.py
+++ django_vendor/templatetags/vendor.py
@@ -1,13 +1,15 @@
 from django.template import Library
-from django_vendor.finders import RemoteFileFinder
 from django.templatetags.static import static
 
+from django_vendor.finders import RemoteFileFinder
+
 register = Library()
+
 
 @register.simple_tag
 def vendor_remote_url(name: str):
     finder = RemoteFileFinder()
-    finder.parse_module_list()
+    finder._parse_module_list()
     for file in finder.files:
         if file.tag_name == name:
             return static(file.file_name)
